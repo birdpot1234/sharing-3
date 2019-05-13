e9835101fbd1b675b90d6a00a9e3d02a231a708e
BirdPeerapon <peerapon.pot@dplusonline.net>
2019-05-13 14:17:23 +0700

    develop api

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,14 @@ router.get('/api/test', function(req, res, next) {
   // res.render('index', { title: 'Express' });
   res.status(200).json({success:false,message:"Success developv3"})
 });
+router.get('/api/test2', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.status(200).json({success:false,message:"Successv2 developv3"})
+});
+router.get('/api/test3', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.status(200).json({success:false,message:"Successv3 developv3"})
+});
 
 
 router.get('/api/v2', function(req, res, next) {
